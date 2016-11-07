37047459183ef979f971f22bc1e74c1fd8d6ac85
Nicola Atzei <atzeinicola@gmail.com>
2016-11-07 17:50:29 +0100

    javadoc: MultipleSessionReceiver

--- co2-honesty-checker/src/main/java/it/unica/co2/api/process/MultipleSessionReceiver.java
+++ co2-honesty-checker/src/main/java/it/unica/co2/api/process/MultipleSessionReceiver.java
@@ -19,6 +19,13 @@ import co2api.SessionI;
 import co2api.TimeExpiredException;
 import it.unica.co2.util.ObjectUtils;
 
+/**
+ * This class allows to receive a {@code Message} from multiple {@code SessionI}.
+ * The algorithm is implemented as polling. 
+ * 
+ * @author Nicola Atzei
+ *
+ */
 public class MultipleSessionReceiver {
 
 	private static final Logger logger = LoggerFactory.getLogger(MultipleSessionReceiver.class);
@@ -26,32 +33,68 @@ public class MultipleSessionReceiver {
 	private Map<SessionI<? extends ContractModel>, List<String>> sessionActionsMap = new HashMap<>();
 	private Map<String, Map<String, Consumer<Message>>> sessionConsumersMap = new HashMap<>();
 	
-	
-	
 	@SuppressWarnings("unused")	private String serializedSessionActionsMap;			// used by JPF
 	
+	/*
+	 * timeout polling
+	 */
+	private static final int POLLING_TIMEOUT = 1000;
 	
-	private static final int WAIT_RECEIVE_TIMEOUT = 1000;
-	
-	
+	/**
+	 * Add the given {@code session}, from which the given {@code actionNames} are expected to come.
+	 * <p>This method is an <em>initialization</em> method.
+	 * 
+	 * @param session The session, already fused or not.
+	 * @param actionNames The action names expected to come from the session
+	 * @return this object
+	 * 
+	 * @see MultipleSessionReceiver#waitForReceive()
+	 * @see MultipleSessionReceiver#waitForReceive(int)
+	 */
 	public MultipleSessionReceiver add(SessionI<? extends ContractModel> session, String... actionNames) {
 		return add(session, (x)->{}, actionNames);
 	}
 	
+	/**
+	 * Add the given {@code session}, from which the given {@code action} is expected to come.
+	 * The given {@code consumer} is executed if the given action is received from the session.
+	 * <p>This method is an <em>initialization</em> method.
+	 * 
+	 * @param session The session, already fused or not.
+	 * @param action The action name expected to come from the session
+	 * @param consumer The consumer to execute if the action is received
+	 * @return this object
+	 * 
+	 * @see MultipleSessionReceiver#waitForReceive()
+	 * @see MultipleSessionReceiver#waitForReceive(int)
+	 */
 	public MultipleSessionReceiver add(SessionI<? extends ContractModel> session, String action, final Consumer<Message> consumer) {
 		return add(session, consumer, new String[]{action});
 	}
 	
+	/**
+	 * Add the given {@code session}, from which the given {@code actionNames} are expected to come.
+	 * The given {@code consumer} is executed if any of the given action is received from the session.
+	 * <p>This method is an <em>initialization</em> method.
+	 * 
+	 * @param session The session, already fused or not.
+	 * @param actionNames The action names expected to come from the session
+	 * @param consumer The consumer to execute if an action is received
+	 * @return this object
+	 * 
+	 * @see MultipleSessionReceiver#waitForReceive()
+	 * @see MultipleSessionReceiver#waitForReceive(int)
+	 */
 	public MultipleSessionReceiver add(SessionI<? extends ContractModel> session, final Consumer<Message> consumer, String... actionNames) {
 
 		logger.debug("adding pair <{}, {}>", session, actionNames);
 		
 		// create a map associating the given consumer with each action
 		
-		HashMap<String, Consumer<Message>> newEntry = new HashMap<>();
+		HashMap<String, Consumer<Message>> newEntries = new HashMap<>();
 		
 		for (String action : actionNames) {
-			newEntry.put(action, consumer);
+			newEntries.put(action, consumer);
 
 			sessionActionsMap.putIfAbsent(session, new ArrayList<>());
 			sessionActionsMap.get(session).add(action);
@@ -59,16 +102,18 @@ public class MultipleSessionReceiver {
 		
 		sessionConsumersMap.merge(
 				session.getContractID(),
-				newEntry, 
+				newEntries, 
 				(x,y)-> {
-					x.putAll(newEntry);
+					x.putAll(newEntries);
 					return x;
 				});
 		
 		return this;
 	}
 	
-	
+	/**
+	 * Start polling between sessions and wait until a specified action come. 
+	 */
 	public void waitForReceive() {
 		try {
 			waitForReceive(-1);
@@ -79,6 +124,12 @@ public class MultipleSessionReceiver {
 		}
 	}
 	
+	/**
+	 * Start polling between sessions and wait until a specified action come or the timeout expires.
+	 *
+	 * @param timeout msec
+	 * @throws TimeExpiredException if the timeout expires
+	 */
 	public void waitForReceive(int timeout) throws TimeExpiredException {
 		
 		try {
@@ -105,7 +156,9 @@ public class MultipleSessionReceiver {
 		consumer.accept(msg);
 	}
 	
-	
+	/*
+	 * internal implementation of the receive
+	 */
 	private Message _waitForReceive(int timeout) throws TimeExpiredException {
 		
 		
@@ -141,17 +194,15 @@ public class MultipleSessionReceiver {
 					
 					try {
 						// wait for receive a message
-						Message msg = session.waitForReceive(WAIT_RECEIVE_TIMEOUT, actions.toArray(new String[]{}));
+						Message msg = session.waitForReceive(POLLING_TIMEOUT, actions.toArray(new String[]{}));
 						return msg;
 					}
 					catch (TimeExpiredException e1) {
-						logger.debug("session {} does not receive any message within the given delay ({} msec)", session, WAIT_RECEIVE_TIMEOUT);
+						logger.debug("session {} does not receive any message within the given delay ({} msec)", session, POLLING_TIMEOUT);
 					}
 				}
-				
 			}
 		}
-		
 	}
 	
 }
